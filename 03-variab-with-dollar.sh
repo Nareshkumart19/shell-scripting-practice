911f3ce1d63ddebcf81869517d0301de603723a2
Nareshkumart19 <kumarnaresht19@gmail.com>
2025-05-17 15:31:56 +0530

    updated code

---
+++ 03-variab-with-dollar.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+
+# conversation with variable 
+
+person1=$1
+person2=$2
+
+echo "$person1:: hi , $person2 how are you "
+echo "$person2:: hi $person1 fine and what about you"
+echo "$person1:: every thing the fine how is you faminy"
+echo "$person2:: all is good , will call u later "
+
+#bash <file name> value1 value2
+#bash <file name>  suresh  "naresh" 
