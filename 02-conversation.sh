62ea7544f023d5147dd37a0de5a79e11be1882ed
Nareshkumart19 <kumarnaresht19@gmail.com>
2025-05-17 14:58:21 +0530

    updated code

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -6,9 +6,9 @@
 name1=$naresh
 name2=$hari
 
-echo "$name1: hi , how are you   $name2"
-echo "$name2; hi im fine and what about you $name2"
-echo "$name1: every thing the fine how is you faminy $name2"
-echo "$name2: all is good , will call u later $name1"
+echo "$name1:: hi , how are you   $name2"
+echo "$name2:: hi im fine and what about you $name2"
+echo "$name1:: every thing the fine how is you faminy $name2"
+echo "$name2:: all is good , will call u later $name1"
 
 
