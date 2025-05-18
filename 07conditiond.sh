02ad2ea544567814bdce6d27b686bfeb6aa6b16c
Nareshkumart19 <kumarnaresht19@gmail.com>
2025-05-18 12:28:46 +0530

    special varibles

--- 07conditiond.sh
+++ 07conditiond.sh
@@ -2,13 +2,12 @@
 
 NUMBER=$1
 
-if[ number -lt 10 ]
+if[ $NUMBER -lt 10 ]
 then
    echo "give the number    $NUMBER less then 10"
 else
     echo " gine the number $NUMBER not less then 10"
 fi    
-
 
 
 
