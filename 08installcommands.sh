7de28acdb77a552997c36545fee58287c498ae79
Nareshkumart19 <kumarnaresht19@gmail.com>
2025-05-18 15:24:26 +0530

    special varibles

--- 08installcommands.sh
+++ 08installcommands.sh
@@ -2,6 +2,7 @@
 
 userid=$(id -u)
 
+# when im runing normal user
 if [ $userid -ne 0 ]
 then
    echo " error run the the script with root acess"
@@ -10,14 +11,35 @@ else
    echo " you are runing with root acess"
 fi
 
-dnf install mysql -y
 
-#dnf remove mysql -y
+# root user
+dnf list installed mysql
 
-if [ $? -eq 0 ]
-then
-    echo "installing musql  is .... sucess"
+if [ $? -ne -0 ]
+    echo "mysql is not installed ...going to install it"
+    dnf install mysql -y
+    if [ $? -eq 0 ]
+    else
+        echo "installing msql .....sucess"
+    else
+        echo "installed mysql is....... fail"
+        exit 1
+    fi
 else
-    echo "installing my sql is ....fail"   
-    exit 1
+    echo " mysql is a;lredy installd..... nothing to do"
 fi    
+
+
+#dnf install mysql -y
+
+##dnf remove mysql -y
+#
+#
+#
+#if [ $? -eq 0 ]
+#then
+#    echo "installing musql  is .... sucess"
+#else
+#    echo "installing my sql is ....fail"   
+#    exit 1
+#fi    
