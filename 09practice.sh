8e8d9a7b97dbfcc4b79b155c76dc43c6e2f0b493
Nareshkumart19 <kumarnaresht19@gmail.com>
2025-05-18 17:09:01 +0530

    new file

--- 09practice.sh
+++ 09practice.sh
@@ -10,30 +10,32 @@ else
     echo " you are running root access"
 fi
 
+#check alredy installed or not . if installed $? is 0, then
+#if not installed $? is not 0. expression is true
 
-
-dnf install mysql -y
-if [ $? -eq 0 ]
-then
-    echo "mysql is inastlled .....sucessfully"
-else
-    echo "mysql is not installed ......failure"
-    exit 1
-fi
-
-
-#dnf list installed  mysql
-#if [ $? -ne 0 ]
+#dnf install mysql -y
+#if [ $? -eq 0 ]
 #then
-#    echo " my sql is not installed  going to  install it"
-#    dnf install mysql -y
-#    if [ $? -eq 0 ]
-#    then
-#        echo "mysql is inastlled .....sucessfully"
-#    else
-#        echo "mysql is not installed ......failure"
-#        exit 1
-#    fi
+#    echo "mysql is inastlled .....sucessfully"
 #else
-#    echo " mysql is alresy installed in your system"
+#    echo "mysql is not installed ......failure"
+#    exit 1
 #fi
+
+
+
+dnf list installed  mysql
+if [ $? -ne 0 ]
+then
+    echo " my sql is not installed  going to  install it"
+    dnf install mysql -y
+    if [ $? -eq 0 ]
+    then
+        echo "mysql is inastlled .....sucessfully"
+    else
+        echo "mysql is not installed ......failure"
+        exit 1
+    fi
+else
+    echo " mysql is alresy installed in your system"
+fi
