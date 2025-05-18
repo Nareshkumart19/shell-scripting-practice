25f5196b14cdc5d29c19e3f1fd5dced29766ad54
Nareshkumart19 <kumarnaresht19@gmail.com>
2025-05-18 12:37:10 +0530

    special varibles

--- 07conditiond.sh
+++ 07conditiond.sh
@@ -4,7 +4,7 @@ number=$1
 
 if [ $number -gt 10 ]
 then
-    echo " gibe the $number greter than 10"
+    echo " give  the $number greter than 10"
 else
     echo " give the $number not greater 10"
 fi
