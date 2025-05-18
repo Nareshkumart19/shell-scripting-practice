1afab24f3cb956fa616d9a147f7b23bfd72b5bcd
Nareshkumart19 <kumarnaresht19@gmail.com>
2025-05-18 11:49:13 +0530

    updated code

--- 05-array.sh
+++ 05-array.sh
@@ -6,3 +6,4 @@ echo "  names : ${devopsgroupteam[2]}"
 echo " all  names : ${devopsgroupteam[@]}"
 
 echo "  names : $devopsgroupteam[2]"
+
---
+++ 06special-variables.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo" All command-line arguments as an array $@"
