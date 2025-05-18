204a90582d0bfdf2bf6662895b53ee0eb54797fe
Nareshkumart19 <kumarnaresht19@gmail.com>
2025-05-18 12:05:37 +0530

    special varibles

--- 06special-variables.sh
+++ 06special-variables.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 echo "All varibles as an array:  $@"
-echo "The name of the script being executed: $0" 
 echo "Number of varibles: $#" 
+echo "The name of the script : $0" 
+echo "current directory $PWD"
+echo "user rununing the script: $USER"
+echo "home directory user $HOME"
+echo "PID OF THE SCRIPT  $$"
+sleep 10 &
+echo " The process ID of the last background command : $!
